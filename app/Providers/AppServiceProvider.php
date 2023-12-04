1bcbfe39fe0e3766f079fc17afac4249ec389f0f
tomoki <tmdesign.0223@gmail.com>
2023-12-04 10:01:05 +0900

    Gateの登録

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use App\Models\Task;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +23,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 登録時
+        Gate::define('store-task', function (User $user) {
+            return $user->id === Auth::id();
+        });
+        // 更新時
+        Gate::define('update-task', function (User $user, Task $task) {
+            return $user->id === $task->user->id;
+        });
+        // 削除時
+        Gate::define('delete-task', function (User $user, Task $task) {
+            return $user->id === $task->user->id;
+        });
     }
 }
