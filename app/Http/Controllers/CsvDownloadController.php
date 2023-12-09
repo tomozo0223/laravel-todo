7f955d65000baef4c44c8c64828f963d59c3a750
tomoki <tmdesign.0223@gmail.com>
2023-12-09 11:35:32 +0900

    ダウンロード処理修正

--- app/Http/Controllers/CsvDownloadController.php
+++ app/Http/Controllers/CsvDownloadController.php
@@ -3,17 +3,37 @@
 namespace App\Http\Controllers;
 
 use App\Models\Task;
+use Illuminate\Http\Request;
 
 class CsvDownloadController extends Controller
 {
-    public function csvDownload(Task $task)
+    public function csvDownload(Request $request)
     {
+        // チェックボックスの値が存在するとき
+        if ($request->selected_tasks) {
+            $taskIds = $request->input('selected_tasks');
+            foreach ($taskIds as $taskId) {
+                $tasks[] = Task::findOrFail($taskId);
+            }
+        } else {
+            $tasks = Task::all();
+        }
         $fileName = 'task.csv';
 
-        $callback = function () use ($task) {
-            $handle = fopen('php://output', 'w');
-            $task = [$task->title, $task->body];
-            fputcsv($handle, $task);
+
+        $callback = function () use ($tasks) {
+            $handle = fopen('php://output', 'a');
+            foreach ($tasks as $task) {
+                $taskData[] = [
+                    $task->title,
+                    $task->body,
+                    $task->user->name,
+                    $task->created_at
+                ];
+            }
+            foreach ($taskData as $data) {
+                fputcsv($handle, $data);
+            }
             fclose($handle);
         };
 
